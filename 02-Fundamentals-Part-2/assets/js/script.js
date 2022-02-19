ba593b2e5605eb00032a8cfbc7cdf94595679d67
Kiran-7 <kiranr.hv@gmail.com>
2022-02-19 22:35:45 +0530

    JavaScript fundamentals 2

--- 02-Fundamentals-Part-2/assets/js/script.js
+++ 02-Fundamentals-Part-2/assets/js/script.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // Activating Strict mode
 
 // let hasDriversLicense = false;
-// const passTest = true; 
+// const passTest = true;
 
 // if (passTest) hasDriversLicense = true;  // Change the variable name to understand that the mistake is shown only on strict mode.
 // if (hasDriversLicense) console.log("I can Drive");
@@ -81,7 +81,6 @@ A variable holds value, but a function can hold one or more complete line of cod
 // console.log(yearsUntillRetirement(2005, "Aryan"));
 // console.log(yearsUntillRetirement(2020, "John"));
 
-
 // ---------------- Functions Calling Other Functions ----------------
 
 // function cutFruitPieces(fruit){
@@ -97,7 +96,6 @@ A variable holds value, but a function can hold one or more complete line of cod
 
 // console.log(fruitProcessor(2,4));
 
-
 // ---------------- Reviewing Functions ----------------
 
 // const calcAge = function(birthYear){
@@ -112,13 +110,12 @@ A variable holds value, but a function can hold one or more complete line of cod
 //         return retirement;
 //     } else {
 //         return -1;
-//     }    
+//     }
 //     // return (`${firstName} is retires in ${retirement} years.`);
 // }
 
 // console.log(yearsUntillRetirement(1993 , "Kiran"));
 // console.log(yearsUntillRetirement(1950 , "Kiran"));
-
 
 // ---------------- Arrays ----------------
 
@@ -181,13 +178,13 @@ A variable holds value, but a function can hold one or more complete line of cod
 // friends.unshift("Chester");
 // console.log(friends);
 
-// // Remove elements 
+// // Remove elements
 
 // // Array Pop method - This removes the last element from the array.
 
 // friends.pop(); //Last element is removed.
 // console.log(friends);
-// const popped = friends.pop(); 
+// const popped = friends.pop();
 // console.log(popped);
 // console.log(friends);
 
@@ -220,13 +217,13 @@ A variable holds value, but a function can hold one or more complete line of cod
 
 // Introduction to Objects
 
-const person = {
-    firstName:"Kiran",
-    lastName:"Ramesh",
-    age:2022-1993,
-    job:"coder",
-    friends:["mike","bob", "jay"]
-}
+// const person = {
+//     firstName:"Kiran",
+//     lastName:"Ramesh",
+//     age:2022-1993,
+//     job:"coder",
+//     friends:["mike","bob", "jay"]
+// }
 
 // how to retrive data from objects using both Dot & bracket notations
 // console.log(person);
@@ -251,8 +248,178 @@ const person = {
 // person["gmail"] = "kiranr.hv@gmail.com";
 // console.log(person);
 
-// Challenge 
+// Challenge
 
 // Person has 3 friends, and his bestfriend is called Mike.
 
-console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is ${person.friends[0]}`);
+// console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is ${person.friends[0]}`);
+
+// Object Methods
+
+// const person = {
+//   firstName: "Mike",
+//   lastName: "Blue",
+//   birthYear: 1993,
+//   job: "coder",
+//   friends: ["Damon", "bob", "jay"],
+//   hasDriversLicense: true,
+
+//   calcAge: (birthYear) => {
+//     return 2035 - birthYear;
+//   },
+
+//   not using arrow functions as we cannot bind this key word to it.
+//   calcAge: function () {
+//     console.log(this);
+//     return 2035 - this.birthYear;
+//   },
+
+// calcAge: function () {
+//   this.age = 2045 - this.birthYear;
+//   return this.age;
+// },
+
+// getSummary: function () {
+//   return `${this.firstName} is a ${this.calcAge()} year old ${
+//     this.job
+//   }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`;
+// },
+// };
+
+// console.log(person.calcAge()); //Dot Notation.
+
+// console.log(person.age); //Dot Notation.
+// console.log(person.age); //Dot Notation.
+// console.log(person.age); //Dot Notation.
+
+// console.log(person["calcAge"](person["birthYear"])); // Bracket Notation.
+
+// Challenge
+
+// Mike is a 50 year old teacher, and he has a/no driver's license
+
+// console.log(person.getSummary());
+
+// Iteration : The For Loop
+
+// Loops are the fundamental aspects of every programming languages,because they basically allow us to automate repetitve tasks.
+
+// console.log("Lifting weights repitition 1 🏋️‍♂️");
+// console.log("Lifting weights repitition 2 🏋️‍♂️");
+// console.log("Lifting weights repitition 3 🏋️‍♂️");
+// console.log("Lifting weights repitition 4 🏋️‍♂️");
+// console.log("Lifting weights repitition 5 🏋️‍♂️");
+// console.log("Lifting weights repitition 6 🏋️‍♂️");
+// console.log("Lifting weights repitition 7 🏋️‍♂️");
+// console.log("Lifting weights repitition 8 🏋️‍♂️");
+// console.log("Lifting weights repitition 9 🏋️‍♂️");
+// console.log("Lifting weights repitition 10 🏋️‍♂️");
+
+//  for loop keeps running while the condition is TRUE.
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repitition ${rep} 🏋️‍♂️`);
+// }
+
+// Looping Arrays, breaking and continuing
+
+// const personArray = [
+//   "Kiran",
+//   "Ramesh",
+//   2022 - 1993,
+//   "coder",
+//   ["mike", "bob", "jay"],
+//   true,
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < personArray.length; i++) {
+//   // Reading from jonas array
+//   console.log(personArray[i], typeof personArray[i]);
+
+//   // Filling types array
+//   // types[i] = typeof personArray[i]; // 1 way of adding element to the array
+
+//   types.push(typeof personArray[i]); // another way of adding element to the array
+// }
+
+// console.log(types);
+
+// const years = [1993, 2005, 1987, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2025 - years[i]);
+// }
+
+// console.log(ages);
+
+// // continue and break
+
+// // continue is to exit the current iteration of the loop and continue to the next one.
+
+// // break is used to compeletly terminate the whole loop
+
+// console.log("----------ONLY STRINGS--------");
+
+// for (let i = 0; i < personArray.length; i++) {
+//   if (typeof personArray[i] !== "string") continue;
+//   console.log(personArray[i], typeof personArray[i]);
+// }
+
+// console.log("---------BREAK WITH NUMBER---------");
+// for (let i = 0; i < personArray.length; i++) {
+//   if (typeof personArray[i] === "number") break;
+//   console.log(personArray[i], typeof personArray[i]);
+// }
+
+// console.log("----------Loop over an array backwords--------");
+
+// // Loop from last index - backwords
+// const personArray = [
+//   "Kiran",
+//   "Ramesh",
+//   2022 - 1993,
+//   "coder",
+//   ["mike", "bob", "jay"],
+//   true,
+// ];
+
+// for (let i = personArray.length - 1; i >= 0; i--) {
+//   console.log(i, personArray[i]);
+// }
+
+// console.log("----------Loops inside loops--------");
+
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`-------Starting exercise ${exercise}-------`);
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+//   }
+// }
+
+console.log("----------Understanding While Loop --------");
+
+// console.log("-----For Loop -----");
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`For Loop --->>>--- Lifting weights repitition ${rep} 🏋️‍♂️`);
+// }
+
+// console.log("-----While Loop -----");
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`While Loop --->>>--- Lifting weights repitition ${rep} 🏋️‍♂️`);
+//   rep++;
+// }
+
+console.log("------Roll of a DICE");
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log("Loop is about to end....");
+  }
+}
